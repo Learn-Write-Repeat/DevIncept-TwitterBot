fcfec086586cead6cd0f422a20a8d626e9719548
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-09-26 12:47:36 +0530

    Reduce delay

--- main.py
+++ main.py
@@ -9,28 +9,6 @@ logger = logging.getLogger()
 from time import sleep
 
 
-# class FavRetweetListener(tweepy.StreamListener):
-#     def __init__(self, api):
-#         self.api = api
-#         self.me = api.me()
-
-#     def on_status(self, tweet):
-#         logger.info(f"Processing tweet id {tweet.id}")
-#         if tweet.in_reply_to_status_id is not None or \
-#                 tweet.user.id == self.me.id:
-#             # This tweet is a reply or I'm its author so, ignore it
-#             return
-#         if not tweet.favorited:
-#             # Mark it as Liked, since we have not done it yet
-#             try:
-#                 tweet.favorite()
-#             except Exception:
-#                 logger.error("Error on fav", exc_info=True)
-
-#     def on_error(self, status):
-#         logger.error(status)
-
-
 def get_tweets(api, search_keywords):
     # Exclude retweets from search to avoid repeats
     tweets = tweepy.Cursor(api.search,
@@ -55,7 +33,7 @@ def like_tweets_now(api, tweets):
                     logger.error("Error on fav", exc_info=True)
             else:
                 logger.info("Has been favorited previously")
-        sleep(100)
+        sleep(30)
 
 
 def main(keywords):
